d4c7141ebde955157099afa87bec39c842addd9a
Furkan IŞIKAY <byfurkan63@gmail.com>
2021-10-25 23:06:19 +0300

    Initial commit

---
+++ ArduinoIlkProjem/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ArduinoIlkProjem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDurum = new System.Windows.Forms.Button();
+            this.pboxDurum = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBaglanti = new System.Windows.Forms.Button();
+            this.pboxBaglanti = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxDurum)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxBaglanti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDurum
+            // 
+            this.btnDurum.Location = new System.Drawing.Point(50, 70);
+            this.btnDurum.Name = "btnDurum";
+            this.btnDurum.Size = new System.Drawing.Size(78, 24);
+            this.btnDurum.TabIndex = 0;
+            this.btnDurum.Text = "Çalıştır";
+            this.btnDurum.UseVisualStyleBackColor = true;
+            this.btnDurum.Click += new System.EventHandler(this.btnDurum_Click);
+            // 
+            // pboxDurum
+            // 
+            this.pboxDurum.BackColor = System.Drawing.Color.Red;
+            this.pboxDurum.Location = new System.Drawing.Point(86, 16);
+            this.pboxDurum.Name = "pboxDurum";
+            this.pboxDurum.Size = new System.Drawing.Size(14, 13);
+            this.pboxDurum.TabIndex = 1;
+            this.pboxDurum.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Led Durumu : ";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnDurum);
+            this.groupBox1.Controls.Add(this.pboxDurum);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(186, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlem";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbPorts);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnBaglanti);
+            this.groupBox2.Controls.Add(this.pboxBaglanti);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(168, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bağlantı";
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(41, 43);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 21);
+            this.cmbPorts.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bağlantı Durumu : ";
+            // 
+            // btnBaglanti
+            // 
+            this.btnBaglanti.Location = new System.Drawing.Point(31, 70);
+            this.btnBaglanti.Name = "btnBaglanti";
+            this.btnBaglanti.Size = new System.Drawing.Size(106, 24);
+            this.btnBaglanti.TabIndex = 0;
+            this.btnBaglanti.Text = "Bağlan";
+            this.btnBaglanti.UseVisualStyleBackColor = true;
+            this.btnBaglanti.Click += new System.EventHandler(this.btnBaglanti_Click);
+            // 
+            // pboxBaglanti
+            // 
+            this.pboxBaglanti.BackColor = System.Drawing.Color.Red;
+            this.pboxBaglanti.Location = new System.Drawing.Point(106, 16);
+            this.pboxBaglanti.Name = "pboxBaglanti";
+            this.pboxBaglanti.Size = new System.Drawing.Size(14, 13);
+            this.pboxBaglanti.TabIndex = 1;
+            this.pboxBaglanti.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 132);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxDurum)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxBaglanti)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDurum;
+        private System.Windows.Forms.PictureBox pboxDurum;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBaglanti;
+        private System.Windows.Forms.PictureBox pboxBaglanti;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ArduinoIlkProjem/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoIlkProjem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPorts.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void btnBaglanti_Click(object sender, EventArgs e)
+        {
+            if (cmbPorts.SelectedIndex != -1)
+            {
+                if (btnBaglanti.Text == "Bağlan")
+                {
+                    btnBaglanti.Enabled = false;
+                    serialPort1.PortName = cmbPorts.SelectedItem.ToString();
+                    serialPort1.Open();
+                    pboxBaglanti.BackColor = Color.Green;
+                    btnBaglanti.Text = "Bağlantıyı Kes";
+                    btnBaglanti.Enabled = true;
+                    groupBox1.Enabled = true;
+                }
+                else
+                {
+                    btnBaglanti.Enabled = false;
+                    pboxBaglanti.BackColor = Color.Red;
+                    serialPort1.Close();
+                    cmbPorts.Items.Clear();
+                    cmbPorts.Items.AddRange(SerialPort.GetPortNames());
+                    btnBaglanti.Text = "Bağlan";
+                    btnBaglanti.Enabled = true;
+                    groupBox1.Enabled = false;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Islem(pboxDurum, () => {
+                pboxBaglanti.BackColor = Color.Red;
+            });
+            serialPort1.Close();
+        }
+
+        private void btnDurum_Click(object sender, EventArgs e)
+        {
+            string text = string.Empty;
+            Islem(btnDurum, () => {
+                text = btnDurum.Text;
+            });
+            serialPort1.Write((text == "Çalıştır") ? "1" : "0");
+            Islem(btnDurum, () => {
+                btnDurum.Enabled = false;
+            });
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string data = sp.ReadExisting();
+            if (data.Contains("1"))
+            {
+                Islem(btnDurum, () => {
+                    btnDurum.Enabled = true;
+                    btnDurum.Text = "Durdur";
+                });
+                Islem(pboxDurum, () => {
+                    pboxDurum.BackColor = Color.Green;
+                });
+            }
+            else if(data.Contains("2"))
+            {
+                Islem(btnDurum, () => {
+                    btnDurum.Enabled = true;
+                    btnDurum.Text = "Çalıştır";
+                });
+                Islem(pboxDurum, () => {
+                    pboxDurum.BackColor = Color.Red;
+                });
+            }
+        }
+
+        public static void Islem(Control ctrl, Action islemler)
+        {
+            if (ctrl != null)
+            {
+                if (ctrl.InvokeRequired)
+                {
+                    try { ctrl.Invoke((MethodInvoker)delegate { islemler(); }); }
+                    catch (ObjectDisposedException) { }
+                    catch (Exception ex) { throw ex; }
+                }
+                else { islemler(); }
+            }
+        }
+    }
+}
